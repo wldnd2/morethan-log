a64fd3fb57d57d82bb868a844e86484f56fcd593
Jiwoong Jeon <100078615+wldnd2@users.noreply.github.com>
2024-06-22 22:27:01 +0900

    Refactor: Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "wldnd2",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Network Engineer",
+    bio: "Happy~",
+    email: "jiwoong000628@gmail.com",
+    linkedin: "",
+    github: "wldnd2",
+    instagram: "dnd._.hihi",
   },
   projects: [
     {
@@ -54,7 +54,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
